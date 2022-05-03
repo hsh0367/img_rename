7aa7eb6c819849c20a36a2d0b4784a07d1cfcb26
hsh0367 <hsh0367@gmail.com>
2022-05-03 13:32:35 +0900

    feat: image rename

---
+++ img_rename.py
@@ -0,0 +1,17 @@
+
+import sys
+import os
+files_path = "./asset/" 
+file_name_and_time_lst = []
+for f_name in os.listdir(f"{files_path}"):
+    written_time = os.path.getctime(f"{files_path}{f_name}")
+    file_name_and_time_lst.append((f_name, written_time))
+sorted_file_lst = sorted(file_name_and_time_lst, key=lambda x: x[1], reverse=True)
+recent_file = sorted_file_lst[0]
+start_int = int(input('숫자를 입력해주세요 : '))
+for image in sorted_file_lst:
+    src = os.path.join(files_path, image[0])
+    dst = str(start_int) + '.png'
+    dst = os.path.join(files_path, dst)
+    os.rename(src, dst)
+    start_int += 1
