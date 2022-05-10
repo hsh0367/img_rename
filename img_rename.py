33663a11da59ca2cd1c943897a6f41aef44775e0
hsh0367 <hsh0367@gmail.com>
2022-05-10 13:26:58 +0900

    fix: 정렬 순서 변경

--- img_rename.py
+++ img_rename.py
@@ -1,15 +1,20 @@
 
 import sys
 import os
+import datetime
 files_path = "./asset/" 
 file_name_and_time_lst = []
 for f_name in os.listdir(f"{files_path}"):
     written_time = os.path.getctime(f"{files_path}{f_name}")
     file_name_and_time_lst.append((f_name, written_time))
-sorted_file_lst = sorted(file_name_and_time_lst, key=lambda x: x[1], reverse=True)
-recent_file = sorted_file_lst[0]
+
+sorted_file_list = sorted(file_name_and_time_lst, key=lambda x: x[1], reverse=False)
+
+recent_file = sorted_file_list[0]
 start_int = int(input('숫자를 입력해주세요 : '))
-for image in sorted_file_lst:
+for image in sorted_file_list:
+    create_time=datetime.datetime.fromtimestamp(image[1])
+    print(f'{image[0]} {create_time}')
     if image[0] != ".gitkeep":
         src = os.path.join(files_path, image[0])
         dst = str(start_int) + '.png'
